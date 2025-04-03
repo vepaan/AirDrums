524e10191dc33c8ceb71019365ad7615dda252e7
vepaan <141347566+vepaan@users.noreply.github.com>
2025-04-02 20:33:11 -0500

    refactoring to python

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,47 @@
 import cv2
 import numpy as np
+import mediapipe
 
-image = cv2.imread("test.png")
-cv2.imshow("AirDrums", image)
-cv2.waitKey(0)
+
+def find_best_cam():
+    max_res, best_idx = 0, -1
+    for i in range(10):
+        temp = cv2.VideoCapture(i)
+        if temp.isOpened():
+            width = temp.get(cv2.CAP_PROP_FRAME_WIDTH)
+            height = temp.get(cv2.CAP_PROP_FRAME_HEIGHT)
+            res = width * height
+            print("Cam ", i, " detected with res: ", width, "x", height)
+
+            if res > max_res:
+                res = max_res
+                best_idx = i
+            
+            temp.release()
+
+    if best_idx == -1:
+        raise Exception("No cam found")
+
+    return best_idx
+
+
+cap = cv2.VideoCapture();
+best_idx = find_best_cam()
+
+cap.open(best_idx)
+if not cap.isOpened():
+    raise Exception("Cannot open camera")
+
+print("Using camera ", best_idx)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    cv2.imshow("AirDrums", frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
 cv2.destroyAllWindows()
